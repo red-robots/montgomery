a8e0088ca1acb96bcd1d7bbc3baebf4da9f02f7c
Lisa <43532676+lisadebona@users.noreply.github.com>
2023-03-22 15:11:36 -0400

    fix mobile view issues

--- inc/scripts.php
+++ inc/scripts.php
@@ -44,7 +44,7 @@ function bellaworks_scripts() {
   wp_enqueue_script( 
     'bellaworks-custom', 
     get_template_directory_uri() . '/assets/js/custom/custom.js', 
-    array(), '20200713', true 
+    array(), '20232203', true 
   );
 
 	wp_localize_script( 'bellaworks-custom', 'frontajax', array(
